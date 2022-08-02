d3b78c7289412deca62d587d03d613c6621ecd47
QuangThanh3001 <quangthanh33200@gmail.com>
2022-08-02 18:10:26 +0700

    function

---
+++ js/basic-functions.js
@@ -0,0 +1,7 @@
+var msg = 'Sign up to receive our newsletter for 10% off';
+
+function updatMessage() {
+    var el = document.getElementById('message');
+    el.textContent = msg;
+}
+updatMessage();
